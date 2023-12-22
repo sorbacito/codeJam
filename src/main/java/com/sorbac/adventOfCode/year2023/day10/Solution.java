bf89476539b4271d063cc6fa159e75a130295fba
skovalcin_pure <skovalcin@purestorage.com>
2023-12-22 22:14:09 +0100

    Solution for AoC 2023 Day 10 https://adventofcode.com/2023/day/10

--- src/main/java/com/sorbac/adventOfCode/year2023/day10/Solution.java
+++ src/main/java/com/sorbac/adventOfCode/year2023/day10/Solution.java
@@ -1,12 +1,13 @@
 package com.sorbac.adventOfCode.year2023.day10;
 
+import com.sorbac.adventOfCode.common.Pair;
+
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
@@ -20,15 +21,14 @@ public class Solution {
         Scanner in1 = new Scanner(new BufferedReader(new InputStreamReader(new FileInputStream("./src/main/java/" + String.join("/", Solution.class.getPackageName().split("\\.")) + "/input1.txt"))));
 //        Scanner in2 = new Scanner(new BufferedReader(new InputStreamReader(new FileInputStream("./src/main/java/" + String.join("/", Solution.class.getPackageName().split("\\.")) + "/test2.txt"))));
 //        Scanner in2 = new Scanner(new BufferedReader(new InputStreamReader(new FileInputStream("./src/main/java/" + String.join("/", Solution.class.getPackageName().split("\\.")) + "/test22.txt"))));
-        Scanner in2 = new Scanner(new BufferedReader(new InputStreamReader(new FileInputStream("./src/main/java/" + String.join("/", Solution.class.getPackageName().split("\\.")) + "/test23.txt"))));
-//        Scanner in2 = new Scanner(new BufferedReader(new InputStreamReader(new FileInputStream("./src/main/java/" + String.join("/", Solution.class.getPackageName().split("\\.")) + "/input2.txt"))));
+//        Scanner in2 = new Scanner(new BufferedReader(new InputStreamReader(new FileInputStream("./src/main/java/" + String.join("/", Solution.class.getPackageName().split("\\.")) + "/test23.txt"))));
+        Scanner in2 = new Scanner(new BufferedReader(new InputStreamReader(new FileInputStream("./src/main/java/" + String.join("/", Solution.class.getPackageName().split("\\.")) + "/input2.txt"))));
 
         List<String> lines1 = new ArrayList<>();
         while (in1.hasNextLine()) {
             lines1.add(in1.nextLine());
         }
-        List<Pos> part1Answer = getPart1Answer(lines1);
-        System.out.println((part1Answer.size() + 1)/2);
+        System.out.println(getPart1Answer(lines1));
 
         List<String> lines2 = new ArrayList<>();
         while (in2.hasNextLine()) {
@@ -38,7 +38,11 @@ public class Solution {
         System.out.println(part2Answer);
     }
 
-    private static List<Pos> getPart1Answer(List<String> lines) {
+    private static int getPart1Answer(List<String> lines) {
+        return (readLoop(lines).size() + 1)/2;
+    }
+
+    private static List<Pos> readLoop(List<String> lines) {
         int depth = 0;
 
         List<List<Pos>> paths = new ArrayList<>();
@@ -58,52 +62,28 @@ public class Solution {
                         Pos p = new Pos(row - 1, col);
                         Collection<Side> sidesP = findSides(lines, p);
                         if (sidesP.contains(Side.SOUTH)) {
-                            if (depth > 1 && getChar(lines, p) == 'S') {
-                                return curP;
-                            } else if (!curP.contains(p)) {
-                                List<Pos> newP = new ArrayList<>(curP);
-                                newP.add(p);
-                                newPaths.add(newP);
-                            }
+                            if (foundLoop(lines, depth, newPaths, curP, p)) return curP;
                         }
                     }
                     if (sideLast == Side.SOUTH) {
                         Pos p = new Pos(row + 1, col);
                         Collection<Side> sidesP = findSides(lines, p);
                         if (sidesP.contains(Side.NORTH)) {
-                            if (depth > 1 && getChar(lines, p) == 'S') {
-                                return curP;
-                            } else if (!curP.contains(p)) {
-                                List<Pos> newP = new ArrayList<>(curP);
-                                newP.add(p);
-                                newPaths.add(newP);
-                            }
+                            if (foundLoop(lines, depth, newPaths, curP, p)) return curP;
                         }
                     }
                     if (sideLast == Side.EAST) {
                         Pos p = new Pos(row, col + 1);
                         Collection<Side> sidesP = findSides(lines, p);
                         if (sidesP.contains(Side.WEST)) {
-                            if (depth > 1 && getChar(lines, p) == 'S') {
-                                return curP;
-                            } else if (!curP.contains(p)) {
-                                List<Pos> newP = new ArrayList<>(curP);
-                                newP.add(p);
-                                newPaths.add(newP);
-                            }
+                            if (foundLoop(lines, depth, newPaths, curP, p)) return curP;
                         }
                     }
                     if (sideLast == Side.WEST) {
                         Pos p = new Pos(row, col - 1);
                         Collection<Side> sidesP = findSides(lines, p);
                         if (sidesP.contains(Side.EAST)) {
-                            if (depth > 1 && getChar(lines, p) == 'S') {
-                                return curP;
-                            } else if (!curP.contains(p)) {
-                                List<Pos> newP = new ArrayList<>(curP);
-                                newP.add(p);
-                                newPaths.add(newP);
-                            }
+                            if (foundLoop(lines, depth, newPaths, curP, p)) return curP;
                         }
                     }
                 }
@@ -114,51 +94,15 @@ public class Solution {
         return List.of();
     }
 
-    private static Collection<Pos> findAdjPos(List<String> lines, Pos p) {
-        List<Pos> ret = new ArrayList<>();
-        int row = p.row();
-        int col = p.col();
-        if (isValid(row + 1, col, lines)) {
-            char c = lines.get(row + 1).charAt(col);
-            if (c == '|' || c == 'J' || c == 'L') {
-                ret.add(new Pos(row + 1, col));
-            }
-            char cur = lines.get(row).charAt(col);
-            if (c == 'S' &&  cur == '|' || cur == 'L' || cur == 'F') {
-                ret.add(new Pos(row + 1, col));
-            }
+    private static boolean foundLoop(List<String> lines, int depth, List<List<Pos>> newPaths, List<Pos> curP, Pos p) {
+        if (depth > 1 && getChar(lines, p) == 'S') {
+            return true;
+        } else if (!curP.contains(p)) {
+            List<Pos> newP = new ArrayList<>(curP);
+            newP.add(p);
+            newPaths.add(newP);
         }
-        if (isValid(row - 1, col, lines)) {
-            char c = lines.get(row - 1).charAt(col);
-            if (c == '|' || c == '7' || c == 'F') {
-                ret.add(new Pos(row - 1, col));
-            }
-            char cur = lines.get(row).charAt(col);
-            if (c == 'S' &&  cur == '|' || cur == 'F' || cur == '7') {
-                ret.add(new Pos(row - 1, col));
-            }
-        }
-        if (isValid(row, col + 1, lines)) {
-            char c = lines.get(row).charAt(col + 1);
-            if (c == '-' || c == '7' || c == 'J') {
-                ret.add(new Pos(row, col + 1));
-            }
-            char cur = lines.get(row).charAt(col);
-            if (c == 'S' &&  cur == '-' || cur == 'J' || cur == 'L') {
-                ret.add(new Pos(row, col + 1));
-            }
-        }
-        if (isValid(row, col - 1, lines)) {
-            char c = lines.get(row).charAt(col - 1);
-            if (c == '-' || c == 'L' || c == 'F') {
-                ret.add(new Pos(row, col - 1));
-            }
-            char cur = lines.get(row).charAt(col);
-            if (c == 'S' &&  cur == '-' || cur == 'J' || cur == 'L') {
-                ret.add(new Pos(row, col - 1));
-            }
-        }
-        return ret;
+        return false;
     }
 
     private static char getChar(List<String> lines, Pos pos) {
@@ -169,8 +113,8 @@ public class Solution {
         return findSides(lines, pos.row, pos.col);
     }
     private static Collection<Side> findSides(List<String> lines, int row, int col) {
-        char c = lines.get(row).charAt(col);
-        if (isValid(row, col, lines) && c != '.') {
+        if (isValid(row, col, lines)) {
+            char c = lines.get(row).charAt(col);
             if (c == 'S') {
                 return List.of(Side.values());
             }
@@ -223,53 +167,59 @@ public class Solution {
     private static long getPart2Answer(List<String> lines) {
         long sum = 0;
 
-        List<Pos> loop = getPart1Answer(lines);
-        List<Pos> outside = new ArrayList<>();
-        int cols = lines.get(0).length();
-        int rows = lines.size();
-        Queue<Pos> startPos = new LinkedList<>(IntStream.range(0, cols).mapToObj(i -> new Pos(0, i)).toList());
-        startPos.addAll(IntStream.range(0, cols).mapToObj(i -> new Pos(rows - 1, i)).toList());
-        startPos.addAll(IntStream.range(0, rows).mapToObj(i -> new Pos(i, 0)).toList());
-        startPos.addAll(IntStream.range(0, rows).mapToObj(i -> new Pos(i, cols - 1)).toList());
-        while (!startPos.isEmpty()) {
-            Pos pos = startPos.poll();
-            if (!loop.contains(pos) && !outside.contains(pos)) {
-                outside.add(pos);
-            }
-            int row = pos.row;
-            int col = pos.col;
-            if (isValid(row + 1, col, lines)) {
-                Pos newP = new Pos(row + 1, col);
-                if (!loop.contains(newP) && !outside.contains(newP)) {
-                    outside.add(newP);
-                    startPos.add(newP);
-                }
-            }
-            if (isValid(row - 1, col, lines)) {
-                Pos newP = new Pos(row - 1, col);
-                if (!loop.contains(newP) && !outside.contains(newP)) {
-                    outside.add(newP);
-                    startPos.add(newP);
-                }
-            }
-            if (isValid(row, col + 1, lines)) {
-                Pos newP = new Pos(row, col + 1);
-                if (!loop.contains(newP) && !outside.contains(newP)) {
-                    outside.add(newP);
-                    startPos.add(newP);
-                }
-            }
-            if (isValid(row, col - 1, lines)) {
-                Pos newP = new Pos(row, col - 1);
-                if (!loop.contains(newP) && !outside.contains(newP)) {
-                    outside.add(newP);
-                    startPos.add(newP);
-                }
-            }
-        }
-
-
-        return rows*cols - outside.size() - loop.size();
+        List<Pos> loop = readLoop(lines);
+        List<Pos> loopNodes = loop.stream()
+                .filter(p -> !List.of('-', '|').contains(getChar(lines, p)))
+                .toList();
+        int loopArea = Math.abs(IntStream.range(0, loopNodes.size())
+                .mapToObj(i -> new Pair<>(loopNodes.get(i), loopNodes.get((i + 1) % loopNodes.size())))
+                .mapToInt(p -> (p.left().row + p.right().row) * (p.left().col - p.right().col))
+                .sum() / 2);
+        int loopSize = loop.size();
+        return loopArea - loopSize/2 + 1;
+//        List<Pos> outside = new ArrayList<>();
+//        int cols = lines.get(0).length();
+//        int rows = lines.size();
+//        Queue<Pos> startPos = new LinkedList<>(IntStream.range(0, cols).mapToObj(i -> new Pos(0, i)).toList());
+//        startPos.addAll(IntStream.range(0, cols).mapToObj(i -> new Pos(rows - 1, i)).toList());
+//        startPos.addAll(IntStream.range(0, rows).mapToObj(i -> new Pos(i, 0)).toList());
+//        startPos.addAll(IntStream.range(0, rows).mapToObj(i -> new Pos(i, cols - 1)).toList());
+//        while (!startPos.isEmpty()) {
+//            Pos pos = startPos.poll();
+//            if (!loop.contains(pos) && !outside.contains(pos)) {
+//                outside.add(pos);
+//            }
+//            int row = pos.row;
+//            int col = pos.col;
+//            if (isValid(row + 1, col, lines)) {
+//                Pos newP = new Pos(row + 1, col);
+//                if (!loop.contains(newP) && !outside.contains(newP)) {
+//                    outside.add(newP);
+//                    startPos.add(newP);
+//                }
+//            }
+//            if (isValid(row - 1, col, lines)) {
+//                Pos newP = new Pos(row - 1, col);
+//                if (!loop.contains(newP) && !outside.contains(newP)) {
+//                    outside.add(newP);
+//                    startPos.add(newP);
+//                }
+//            }
+//            if (isValid(row, col + 1, lines)) {
+//                Pos newP = new Pos(row, col + 1);
+//                if (!loop.contains(newP) && !outside.contains(newP)) {
+//                    outside.add(newP);
+//                    startPos.add(newP);
+//                }
+//            }
+//            if (isValid(row, col - 1, lines)) {
+//                Pos newP = new Pos(row, col - 1);
+//                if (!loop.contains(newP) && !outside.contains(newP)) {
+//                    outside.add(newP);
+//                    startPos.add(newP);
+//                }
+//            }
+//        }
     }
 
     private record Pos(int row, int col) implements Comparable<Pos> {
