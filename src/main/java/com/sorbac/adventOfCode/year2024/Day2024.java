16d48ae99d5083b54f70a0a0ff4f096ec68ac4cb
Stanislav Kovalcin <stanislas.kovalcin@gmail.com>
2024-11-30 16:47:37 +0100

    Prepare for AoC 2024

---
+++ src/main/java/com/sorbac/adventOfCode/year2024/Day2024.java
@@ -0,0 +1,13 @@
+package com.sorbac.adventOfCode.year2024;
+
+import com.sorbac.adventOfCode.common.Day;
+
+public abstract class Day2024 extends Day {
+    protected Day2024(int day) {
+        super(2015, day);
+    }
+
+    public Day2024(int day, String input) {
+        super(2015, day, input);
+    }
+}
